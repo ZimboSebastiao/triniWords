c5eed27ccb0c017387baec795a709cade3816b50
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-10-30 20:37:37 -0300

    up

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Search, BookText, Settings, MessageCircle } from "lucide-react-native";
+
 import Home from "./src/screens/Home";
 import Settings from "./src/screens/Settings";
 import Search from "./src/screens/Search";
