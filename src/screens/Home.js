832fe86e7d7950dc2eea35d00c2fec2ea243a1d5
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-02 00:58:35 -0300

    up

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <View style={styles.saudacao}>
-        <Text>Hi There 👋</Text>
+        <Text style={styles.title}>Hi There 👋</Text>
+        <Text style={styles.subtitle}>
+          What would you like to train today? Search beloww.
+        </Text>
       </View>
     </View>
   );
@@ -16,8 +19,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#EDEFF0",
   },
   saudacao: {
-    backgroundColor: "red",
     marginVertical: 60,
     alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Roboto-Bold",
+    marginBottom: 10,
+    color: "#30333C",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    color: "#b1b4b5",
+    width: "65%",
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
