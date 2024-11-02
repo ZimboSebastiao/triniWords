cbe71a7182546495ebbd131027937aaefcc6940b
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-02 00:19:34 -0300

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EDEFF0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
