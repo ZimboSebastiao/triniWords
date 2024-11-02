2384b68d3d7d16a7d9881eb0ad631f9aa87f1534
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-02 00:39:59 -0300

    update

--- App.js
+++ App.js
@@ -1,92 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Home from "./src/screens/Home";
-import SettingsScreen from "./src/screens/SettingsScreen";
-import SearchScreen from "./src/screens/SearchScreen";
-import Chatbot from "./src/screens/Chatbot";
-import DicionaryScreen from "./src/screens/DicionaryScreen";
+// App.js
 
-const Tab = createBottomTabNavigator();
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppLoading from "expo-app-loading";
+
+import useLoadFonts from "./src/utils/useLoadFonts";
+import TabNavigator from "./src/navigation/TabNavigator";
 
 export default function App() {
+  const fontsLoaded = useLoadFonts();
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: "#EDEFF0",
-            borderTopColor: "#EDEFF0",
-            height: 55,
-          },
-          tabBarActiveTintColor: "#3BB3BD",
-          tabBarInactiveTintColor: "#b1b4b5",
-          tabBarShowLabel: false,
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={35} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="magnify" color={color} size={35} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Dicionary"
-          component={DicionaryScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="book-open-page-variant"
-                color={color}
-                size={35}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Chatbot"
-          component={Chatbot}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="chat" color={color} size={35} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="cog" color={color} size={35} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#EDEFF0",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
