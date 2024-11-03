dcd47ed3205e40c7ea12b2237d3a95512895ed40
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-02 21:23:44 -0300

    Update DicionaryScreen.js

--- src/screens/DicionaryScreen.js
+++ src/screens/DicionaryScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function DicionaryScreen() {
   return (
-    <View>
-      <Text>DicionaryScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View>
+        <MaterialCommunityIcons
+          name="arrow-left-thin"
+          color="#3BB3BD"
+          size={35}
+        />
+        <MaterialCommunityIcons
+          name="arrow-left-thin"
+          color="#3BB3BD"
+          size={35}
+        />
+      </View>
+    </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#EDEFF0",
+    // justifyContent: "center",
+    padding: 35,
+  },
+});
