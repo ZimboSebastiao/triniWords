8843215a8be779e3ecad3efaa1e1f2cd9dee02ee
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-05 21:34:27 -0300

    Update Splash.js

---
+++ src/screens/Splash.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { Video } from "expo-av";
+
+export default function Splash({ navigation }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate("Login");
+    }, 20000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Video
+        source={require("../../assets/videos/splash.mov")}
+        shouldPlay
+        isLooping
+        resizeMode="contain"
+        style={styles.video}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    flex: 1,
+    width: "45%",
+    height: "45%",
+  },
+});
