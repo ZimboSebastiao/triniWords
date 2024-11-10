f4602c17ceedd1fb3f0f9a88b0bf2ff44b2097be
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-10 14:50:06 -0300

    Update randomWordApi.js

--- src/apis/randomWordApi.js
+++ src/apis/randomWordApi.js
@@ -1,14 +1,12 @@
-import * as Sentry from "@sentry/react-native"; // Importe o Sentry
+import * as Sentry from "@sentry/react-native";
 
 export const fetchRandomWord = async () => {
   try {
-    // Obtendo uma palavra aleatória
     const response = await fetch(
       "https://random-word-api.herokuapp.com/word?number=1"
     );
 
     if (!response.ok) {
-      // Captura erro de status de resposta
       const errorMessage = `Erro ao buscar palavra aleatória: ${response.statusText}`;
       console.error(errorMessage);
       Sentry.captureMessage(errorMessage);
@@ -17,7 +15,6 @@ export const fetchRandomWord = async () => {
 
     const wordArray = await response.json();
     if (!wordArray || wordArray.length === 0) {
-      // Captura caso a resposta seja um array vazio
       const errorMessage = "Nenhuma palavra foi retornada pela API.";
       console.error(errorMessage);
       Sentry.captureMessage(errorMessage);
@@ -26,13 +23,11 @@ export const fetchRandomWord = async () => {
 
     const word = wordArray[0];
 
-    // Obtendo a definição da palavra
     const definitionResponse = await fetch(
       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     );
 
     if (!definitionResponse.ok) {
-      // Captura erro de status de resposta da definição
       const errorMessage = `Erro ao buscar definição da palavra ${word}: ${definitionResponse.statusText}`;
       console.error(errorMessage);
       Sentry.captureMessage(errorMessage);
@@ -41,7 +36,6 @@ export const fetchRandomWord = async () => {
 
     const definition = await definitionResponse.json();
 
-    // Verifica se a definição foi retornada corretamente
     if (!definition || !definition[0]) {
       const errorMessage = `Definição não encontrada para a palavra ${word}.`;
       console.error(errorMessage);
@@ -51,9 +45,8 @@ export const fetchRandomWord = async () => {
 
     return definition;
   } catch (error) {
-    // Captura qualquer erro geral e envia para o Sentry
     console.error("Erro ao buscar palavra aleatória:", error);
-    Sentry.captureException(error); // Envia o erro para o Sentry
+    Sentry.captureException(error);
     return null;
   }
 };
