b15c17e3e57802ca20c5e470f5b17822a6119959
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-11-18 14:57:36 -0300

    Update Splash.js

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,26 +1,28 @@
 import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import { Video } from 'expo-video';
+import { useVideoPlayer, VideoView } from 'expo-video';
 import { navigate } from "../utils/RootNavigation";
 
 export default function Splash() {
+  // Usando o hook useVideoPlayer para controlar o player de vídeo
+  const videoSource = require('../../assets/videos/splash.mp4');
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.play();
+  });
+
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate("Home");
-    }, 1000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Video
-        source={require('../../assets/videos/splash.mp4')}
-        shouldPlay
-        isLooping
-        resizeMode="contain"
-        style={styles.video}
-      />
+      {/* Usando o VideoView para renderizar o vídeo */}
+      <VideoView style={styles.video} player={player} />
     </View>
   );
 }
