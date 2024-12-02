b01380f8892c4a23d07205af330de684337f52e2
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-12-02 19:49:35 -0300

    Update notifications.js

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,16 +1,30 @@
 import * as Notifications from "expo-notifications";
 import { requestNotificationPermissions } from "./notificationsRequest";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export async function scheduleDailyNotification() {
   const hasPermission = await requestNotificationPermissions();
   if (!hasPermission) return;
 
   try {
+    // Verificar se a notificação já foi agendada
+    const notificationScheduled = await AsyncStorage.getItem(
+      "notificationScheduled"
+    );
+
+    if (notificationScheduled === "true") {
+      console.log("Notificação diária já foi agendada.");
+      return; // Não agenda novamente
+    }
+
+    // Cancelar notificações agendadas anteriores
     await Notifications.cancelAllScheduledNotificationsAsync();
 
+    // Agendar nova notificação
     await Notifications.scheduleNotificationAsync({
       content: {
-        title: "Good Morning! 🌞",
-        body: "Let's learn a new word! 📚",
+        title: "Let's learn! 👩‍🏫",
+        body: "Let's learn a new word today! 📚",
         data: { screen: "LearnScreen" },
       },
       trigger: {
@@ -18,8 +32,10 @@ export async function scheduleDailyNotification() {
         minute: 0,
         repeats: true,
       },
-      // trigger: { seconds: 1 },
     });
+
+    // Marcar que a notificação foi agendada
+    await AsyncStorage.setItem("notificationScheduled", "true");
 
     console.log("Notificação diária agendada para às 8h!");
   } catch (error) {
